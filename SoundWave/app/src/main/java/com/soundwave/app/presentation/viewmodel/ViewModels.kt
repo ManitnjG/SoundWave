19b29a854502adb7c9e1ecfdabc45f4ad9d62adb
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2026-06-04 11:50:52 +0530

    Mark ViewModels.kt as obsolete - use individual files instead

---
+++ SoundWave/app/src/main/java/com/soundwave/app/presentation/viewmodel/ViewModels.kt
@@ -0,0 +1,6 @@
+# This file has been split into separate files
+# - SettingsViewModel.kt in com.soundwave.app.presentation.ui.settings
+# - PlaylistViewModel.kt in com.soundwave.app.presentation.ui.playlist
+# - DownloadsViewModel.kt in com.soundwave.app.presentation.ui.downloads
+#
+# This file can be safely deleted as all ViewModels have been moved to their respective package directories.
